b3ddfca65959d3b98b34971ca799deb5aa547312
Rudra <rudra@git>
2025-10-15 10:02:03 +0530

    Initial Commit

---
+++ MacroLensTests/MacroLensTests.swift
@@ -0,0 +1,17 @@
+//
+//  MacroLensTests.swift
+//  MacroLensTests
+//
+//  Created by Rudra on 15/10/25.
+//
+
+import Testing
+@testable import MacroLens
+
+struct MacroLensTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
